b17ab4df97ff3210ba3eeac860e1db9687069171
Thamindu Weerarathne <thamindusandeepa678@gmail.com>
2025-06-08 00:28:48 +0530

    add dotenv.config

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
-
+dotenv.config();
 export function registerUser(req,res){
 
     const data = req.body;
